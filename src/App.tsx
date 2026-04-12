e2b3f54902b91689cb626dbdbbd96d2716aaad08
Sanjeev Midda <s.midda1@gmail.com>
2026-04-12 19:48:14 +0100

    created custom hook for API logic, and increase readability of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,10 @@
-import { useEffect, useState } from "react";
 import "./index.css";
-import type { User } from "./types/user";
+import { useState } from "react";
+import { useUsers } from "./hooks/useUsers";
+
 function App() {
-  // DONE - fetch data from API
-  // DONE - store data from request in state
-  // DONE - set up loading state
-  // DONE - display key user information
-  // DONE - create TS interface for API data
-  // state typed correctly
-  // DONE - set up searching/filtering
-  // handle loading and error states
-
-  // store user data
-  const [userData, setUserData] = useState<User[]>([]);
-  const [status, setStatus] = useState<"loading" | "success" | "error">(
-    "loading"
-  );
   const [userInput, setUserInput] = useState("");
-
-  // fetch data from API
-  useEffect(() => {
-    const controller = new AbortController();
-
-    const getUsers = async () => {
-      setStatus("loading");
-
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-          { signal: controller.signal }
-        );
-
-        if (!response.ok) {
-          throw new Error(
-            `Something went wrong! HTTP error: ${response.status}`
-          );
-        }
-
-        const data = await response.json();
-        setUserData(data);
-        setStatus("success");
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          if (error.name === "AbortError") return;
-          console.error(error.message);
-          setStatus("error");
-        } else {
-          console.error("Unknown error:", error);
-        }
-      }
-    };
-
-    getUsers();
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
+  const { userData, status } = useUsers();
 
   // function to save user input
   const saveUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,10 +13,8 @@ function App() {
 
   // filter users
   const filteredUser = userData.filter((user) =>
-    user.name.toLowerCase().includes(userInput.toLowerCase())
+    (user?.name ?? "").toLowerCase().includes(userInput.toLowerCase())
   );
-
-  console.log(filteredUser);
 
   return (
     <div className="App">
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import type { User } from "../types/user";
+
+export const useUsers = () => {
+  // store user data
+  const [userData, setUserData] = useState<User[]>([]);
+  const [status, setStatus] = useState<"loading" | "success" | "error">(
+    "loading"
+  );
+
+  // fetch data from API
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      setStatus("loading");
+
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+          { signal: controller.signal }
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Something went wrong! HTTP error: ${response.status}`
+          );
+        }
+
+        const data = await response.json();
+        setUserData(data);
+        setStatus("success");
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          if (error.name === "AbortError") return;
+          console.error(error.message);
+          setStatus("error");
+        } else {
+          console.error("Unknown error:", error);
+        }
+      }
+    };
+
+    getUsers();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { userData, status };
+};
