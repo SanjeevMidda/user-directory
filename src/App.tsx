e9d9fa16e78fc71a020c173b097dfb2886e9453c
Sanjeev Midda <s.midda1@gmail.com>
2026-04-12 12:49:35 +0100

    creating async function to get users from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import "./index.css";
 
 function App() {
+  // fetch data from API
+  // display key user information
+  // set up searching/filtering
+  // handle loading and error states
+
+  // fetch data from API
+
+  let getUsers = async () => {
+    let request = await fetch("https://jsonplaceholder.typicode.com/users");
+    let response = await request.json();
+    console.log(response);
+  };
+
+  getUsers();
+
   return <div className="App"></div>;
 }
 
