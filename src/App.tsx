df9ad5a01d576df2a8bfcd0fccbae535465f2bd4
Sanjeev Midda <s.midda1@gmail.com>
2026-04-12 13:37:47 +0100

    changed let to const for safety

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { request } from "http";
 
 function App() {
   // fetch data from API
+  // store data from request in state
   // display key user information
   // set up searching/filtering
   // handle loading and error states
@@ -15,7 +16,7 @@ function App() {
 
     const getUsers = async () => {
       try {
-        let response = await fetch(
+        const response = await fetch(
           "https://jsonplaceholder.typicode.com/users",
           { signal: controller.signal }
         );
@@ -26,11 +27,11 @@ function App() {
           );
         }
 
-        let data = await response.json();
+        const data = await response.json();
         console.log(data);
       } catch (error: unknown) {
         if (error instanceof Error) {
-          if (error.name === "Abort Error") return;
+          if (error.name === "AbortError") return;
           console.error(error.message);
         } else {
           console.error(`Unknown error: ${error}`);
